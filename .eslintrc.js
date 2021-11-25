43688367debb6e6669000a403bc971ff0c4ff288
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-11-25 23:07:45 +0100

    chore: install and configure eslint-plugin-react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,9 +18,11 @@ module.exports = {
     ecmaVersion: 13,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
     'prettier/prettier': 'error',
     'no-console': 'error',
   },
