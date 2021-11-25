7622f7bf53b7ce9c1b6c645da1b06394929bd2ab
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-11-25 16:38:24 +0100

    feat: emtpy state when color was not collected yet

--- app/routes/colors/$id/index.tsx
+++ app/routes/colors/$id/index.tsx
@@ -1,4 +1,4 @@
-import { json, useLoaderData } from 'remix';
+import { json, useLoaderData, Link } from 'remix';
 import type { LoaderFunction } from 'remix';
 import { prisma } from '~/utils/db.server';
 import { requireColor } from '~/utils/colors';
@@ -34,14 +34,23 @@ export default function ColorsIdIndex() {
   const { colorNames } = useLoaderData<ColorsIdIndexData>();
   return (
     <>
-      <p>It&apos;s known by many names:</p>
-      <ul>
-        {colorNames.map(({ name, glossarist: { username } }) => (
-          <li key={username}>
-            {name} by {username}
-          </li>
-        ))}
-      </ul>
+      {colorNames.length === 0 ? (
+        <p>
+          This color was not named yet. Be the first to{' '}
+          <Link to="collect">collect</Link>!
+        </p>
+      ) : (
+        <>
+          <p>It&apos;s known by those names:</p>
+          <ul>
+            {colorNames.map(({ name, glossarist: { username } }) => (
+              <li key={username}>
+                {name} by {username}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 }
