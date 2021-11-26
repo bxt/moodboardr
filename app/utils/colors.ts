319e7e92c7802643e6d40dac7f1da91a91eeab6a
Bernhard Häussner <bernhard.haeussner@xing.com>
2021-11-26 18:30:29 +0100

    fix: show 404 when color has too many digits as well

--- app/utils/colors.ts
+++ app/utils/colors.ts
@@ -1,7 +1,7 @@
 import type { Params } from 'react-router-dom';
 
 export function requireColor(params: Params) {
-  if (!params.id?.match?.(/[0-9a-f]{6}/)) {
+  if (!params.id?.match?.(/^[0-9a-f]{6}$/)) {
     throw new Response('Color must consist of 6 lower case hex digits.', {
       status: 404,
     });
